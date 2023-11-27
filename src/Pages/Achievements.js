12079b4f1949faf1dfe09fe4ae84d96081986fcc
Wendy <wendy.pasiah@ashesi.edu.gh>
2023-11-27 15:53:44 +0000

    Add files

--- src/Pages/Achievements.js
+++ src/Pages/Achievements.js
@@ -1,28 +1,97 @@
 import '../Styles/Achievements.css';
 import { Link } from "react-router-dom";
-
 import React from 'react';
+import {useState} from "react";
+import axios from "axios";
+
+function SuccessMessage() {
+  return (
+    <div className="success-message">
+      <p>Your data has been successfully saved!</p>
+    </div>
+  );
+}
+
+function ErrorMessage(props) {
+  return (
+    <div className="error-message">
+      <p>{props.message}</p>
+    </div>
+  );
+}
 
 
 const Achievements=()=>{
-return (
+  const [formData, setFormData] = useState({
+    achievement_name:"",
+    organization_name:"",
+    year_attained:"",
+  });
+   // Handle form data changes
+   const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const [formSubmitted, setFormSubmitted] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+
+
+  // Handle form submission
+    // Handle form submission
+    const handleSubmit = async (event) => {
+      event.preventDefault();
+      try {
+        const response = await axios.post("http://localhost/finaleproject/LogInSignUp/achievements.php", formData);
+        
+        console.log(response);
+        setFormSubmitted(true);
+      } catch (error) {
+        console.log(error);
+        setErrorMessage("There was an error submitting your data.");
+      }
+    };
+   
+
+
+return (  
+        <form onSubmit={handleSubmit}>
+            {formSubmitted && <SuccessMessage />}
+            {errorMessage && <ErrorMessage message={errorMessage} />}
         <div className='main-archive'>
               <div className="form-archive-row">
                   <div className="form-group col-md-6">
-                    <label for="inputEmail4">Achievement</label>
-                    <input type="text" className="form-control" id="inputEmail4"/>
+                    <label htmlFor="achievement_name">Achievement</label>
+                    <input type="text" className="form-control" 
+                    id="achievement_name"
+                    name="achievement_name"
+                    value={formData.achievement_name}
+                    onChange={handleInputChange}
+                    
+
+                    />
                   </div>
 
                   <div className="form-group col-md-6">
-                    <label for="inputEmail">Organization</label>
-                    <input type="text" className="form-control" id="inputEmail4" />
+                    <label htmlFor="organization_name">Organization</label>
+                    <input type="text"className="form-control" 
+                    id="organization_name"
+                    name="organization_name"
+                    value={formData.organization_name}
+                    onChange={handleInputChange}
+                    />
                   </div>
               </div>
 
               <div className="form-project-row">
                   <div className="form-group col-md-6">
-                    <label for="inputEmail4">Year Attained</label>
-                    <input type="text" className="form-control" id="inputEmail4"/>
+                    <label htmlFor="year_attained">Year Attained</label>
+                    <input type="text" className="form-control" 
+                    id="year_attained"
+                    name="year_attained"
+                    value={formData.year_attained}
+                    onChange={handleInputChange}
+                    />
                   </div>
 
                   <div className="form-project-row">
@@ -39,7 +108,9 @@ return (
                       <button type="submit" className="archive-btn btn-primary">save</button>
                 </div>
               </div>
-          </div>
+            </div>
+            </form>
+         
 
     )
   }
